327d38d02d450c04432bbe5448cfc53e083e234b
carlosb <carlos.benner.b@gmail.com>
2021-03-30 20:56:48 -0300

    FindTheOldest exercise completed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-let findTheOldest = function() {
+let findTheOldestWithForEach = function (peopleArray) {
+    let oldestPerson = peopleArray[0];
+    peopleArray.forEach((person) => {
+        if (getPersonAge(oldestPerson) < getPersonAge(person))
+            oldestPerson = person;
+    });
+    return oldestPerson;
+};
 
+let findTheOldestWithSort = function (peopleArray) {
+    return peopleArray.sort(
+        (person1, person2) => getPersonAge(person2) - getPersonAge(person1)
+    )[0];
+};
+
+function getPersonAge(person) {
+    return person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : 2021 - person.yearOfBirth;
 }
 
-module.exports = findTheOldest
+let findTheOldestWithReduce = function (peopleArray) {
+    return peopleArray.reduce((oldestPerson, person) => {
+        if (getPersonAge(oldestPerson) < getPersonAge(person)) {
+            oldestPerson = person;
+        }
+        return oldestPerson;
+    });
+};
+
+findTheOldest = findTheOldestWithReduce;
+
+module.exports = findTheOldest;
