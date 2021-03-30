45303cb290a21725f3a1a6a85b94beb37e880e9c
carlosb <carlos.benner.b@gmail.com>
2021-03-30 18:44:08 -0300

    Fibonacci exercise completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
+const fibonacci = function (eleNum) {
+    if (!Number.isInteger(+eleNum) || +eleNum < 0) return 'ERROR';
+    if (eleNum == 0) return 0;
+    if (eleNum == 1) return 1;
+    return fibonacci(eleNum - 1) + fibonacci(eleNum - 2);
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
