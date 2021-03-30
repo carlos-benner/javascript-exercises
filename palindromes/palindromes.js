53c685e7f76a32c3a2cb1efc1b29fe3903a24d01
carlosb <carlos.benner.b@gmail.com>
2021-03-30 18:50:57 -0300

    Palindrome exercise completed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.replace(/[^0-9a-z]/gi, '');
+    return word == word.split('').reverse().join('');
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
